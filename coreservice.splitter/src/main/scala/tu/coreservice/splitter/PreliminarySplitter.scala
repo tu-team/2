977a3613479dbc9654d8edcc459afd6c0d1f8ac2
alexander-toschev <sanchis.no@gmail.com>
2012-06-01 19:39:22 +0400

    Corrector done

---
+++ coreservice.splitter/src/main/scala/tu/coreservice/splitter/PreliminarySplitter.scala
@@ -0,0 +1,32 @@
+package tu.coreservice.splitter
+
+import tu.coreservice.action.way2think.Way2Think
+import tu.model.knowledge.communication.Context
+import tu.model.knowledge.frame.TransFrame
+import tu.model.knowledge.Resource
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: toscheva
+ * Date: 01.06.12
+ * Time: 18:59
+ *
+ */
+
+/**
+ * split text in sentence
+ * https://github.com/development-team/2/blob/master/doc/design-specification/splitting-text-to-words.md
+ */
+class PreliminarySplitter extends Way2Think {
+
+  /**
+   * apply way 2 think
+   * @param inputContext
+   * @param outputContext
+   * @return
+   */
+  def apply(inputContext: Context, outputContext: Context): TransFrame[Resource] = {
+    inputContext.
+       return null
+  }
+}
---
+++ model.knowledge/src/main/scala/tu/model/knowledge/narrative/AnnotatedNarrative.scala
@@ -0,0 +1,15 @@
+package tu.model.knowledge.narrative
+
+import tu.model.knowledge.Resource
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: toscheva
+ * Date: 01.06.12
+ * Time: 19:30
+ *
+ */
+
+/*case class AnnotatedNarrative[Type <: Resource](var _words) extends Narrative() {
+
+} */
