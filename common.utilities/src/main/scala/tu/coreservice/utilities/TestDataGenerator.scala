d3c9c45c9d6f9a51b5a3db05d336fdee088d8b5a
Max Talanov <max.talanov@gmail.com>
2012-06-16 16:00:59 +0400

    TU-50 - Create prototype project plan. Started TestDataGenerator.

---
+++ common.utilities/src/main/scala/tu/coreservice/utilities/TestDataGenerator.scala
@@ -0,0 +1,74 @@
+package tu.coreservice.utilities
+
+import tu.model.knowledge.domain.{ConceptLink, Concept}
+import tu.model.knowledge.annotator.{AnnotatedNarrative, AnnotatedPhrase}
+import tu.model.knowledge.{KnowledgeURI, Probability}
+
+/**
+ * Test data generator object.
+ * @author talanov max
+ *         date 2012-06-16
+ *         time: 3:55 PM
+ */
+
+object TestDataGenerator {
+  /**
+   * Simulation structures
+   */
+  val namespace = "2/concepts/"
+  val revision = "0.0"
+
+  /**
+   * concepts
+   */
+  val subject = Concept("subject")
+  val objectConcept = Concept("object")
+  val userConcept = Concept("user")
+  val computerConcept = Concept("computer")
+  val softwareConcept = Concept("sofware")
+  val photoShop = Concept.createSubConcept(softwareConcept, "Adobe Photoshop")
+  val firefox = Concept.createSubConcept(softwareConcept, "Mozilla Firefox")
+  val network = Concept("network")
+  val internet = Concept.createSubConcept(network, "internet")
+  val sharedResources = Concept("sharedResources")
+  val sharedDisk = Concept.createSubConcept(sharedResources, "sharedDisk")
+  val account = Concept("account")
+
+  // actions
+  val action = Concept("action")
+  val installConcept = Concept.createSubConcept(action, "install")
+  val remove = Concept.createSubConcept(action, "remove")
+  val clean = Concept.createSubConcept(action, "clean")
+
+  /**
+   * links
+   */
+  val has = ConceptLink(subject, objectConcept, "has")
+  val userCompLinkedPair = ConceptLink.likConcepts(has, userConcept, computerConcept)
+  val hasComputer = ConceptLink.createSubConceptLink(has, userConcept, computerConcept, "hasComputer", new Probability(1.0, 1.0))
+  val userComputerLinkedPair = ConceptLink.likConcepts(hasComputer, userConcept, computerConcept)
+  val hasSoftware = ConceptLink.createSubConceptLink(has, computerConcept, softwareConcept, "hasSoftware", new Probability(1.0, 0.9))
+  val computerSoftwareLinkedPair = ConceptLink.likConcepts(hasSoftware, computerConcept, softwareConcept)
+  val hasAccount = ConceptLink.createSubConceptLink(has, userConcept, account, "hasAccount", new Probability(1.0, 0.9))
+  val userAccountLinkedPair = ConceptLink.likConcepts(has, userConcept, account)
+
+  /**
+   * phrases
+   */
+  val userPhrase = AnnotatedPhrase("user", userConcept)
+  val customerPhrase = AnnotatedPhrase("customer", userConcept)
+  val notebookPhrase = AnnotatedPhrase("notebook", computerConcept)
+  val laptopPhrase = AnnotatedPhrase("laptop", computerConcept)
+  val desktopPhrase = AnnotatedPhrase("desktop", computerConcept)
+  val computerPhrase = AnnotatedPhrase("computer", computerConcept)
+
+
+  /**
+   * Test phrases
+   */
+  // Please install Firefox
+  val please = AnnotatedPhrase("Please")
+  val install = AnnotatedPhrase("install")
+  val fireFox = AnnotatedPhrase("FireFox")
+  val pleaseInstallFF = AnnotatedNarrative(List(please, install, fireFox), KnowledgeURI("pleaseInstallFF"))
+}
--- coreservice.action.way2think/src/test/scala/tu/coreservice/action/way2think/SimulationTest.scala
+++ coreservice.action.way2think/src/test/scala/tu/coreservice/action/way2think/SimulationTest.scala
@@ -9,73 +9,11 @@ package tu.coreservice.action.way2think
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.FunSuite
-import tu.model.knowledge.domain.{ConceptLink, Concept}
-import tu.model.knowledge.annotator.{AnnotatedNarrative, AnnotatedPhrase}
-import tu.model.knowledge.semanticnetwork.SemanticNetwork
-import tu.model.knowledge.{KnowledgeURI, Probability}
 
 @RunWith(classOf[JUnitRunner])
 class SimulationTest extends FunSuite {
 
-  /**
-   * Simulation structures
-   */
-  val namespace = "2/concepts/"
-  val revision = "0.0"
 
-  /**
-   * concepts
-   */
-  val subject = Concept("subject")
-  val objectConcept = Concept("object")
-  val userConcept = Concept("user")
-  val computerConcept = Concept("computer")
-  val softwareConcept = Concept("sofware")
-  val photoShop = Concept.createSubConcept(softwareConcept, "Adobe Photoshop")
-  val firefox = Concept.createSubConcept(softwareConcept, "Mozilla Firefox")
-  val network = Concept("network")
-  val internet = Concept.createSubConcept(network, "internet")
-  val sharedResources = Concept("sharedResources")
-  val sharedDisk = Concept.createSubConcept(sharedResources, "sharedDisk")
-  val account = Concept("account")
-
-  // actions
-  val action = Concept("action")
-  val installConcept = Concept.createSubConcept(action, "install")
-  val remove = Concept.createSubConcept(action, "remove")
-  val clean = Concept.createSubConcept(action, "clean")
-
-  /**
-   * links
-   */
-  val has = ConceptLink(subject, objectConcept, "has")
-  val userCompLinkedPair = ConceptLink.likConcepts(has, userConcept, computerConcept)
-  val hasComputer = ConceptLink.createSubConceptLink(has, userConcept, computerConcept, "hasComputer", new Probability(1.0, 1.0))
-  val userComputerLinkedPair = ConceptLink.likConcepts(hasComputer, userConcept, computerConcept)
-  val hasSoftware = ConceptLink.createSubConceptLink(has, computerConcept, softwareConcept, "hasSoftware", new Probability(1.0, 0.9))
-  val computerSoftwareLinkedPair = ConceptLink.likConcepts(hasSoftware, computerConcept, softwareConcept)
-  val hasAccount = ConceptLink.createSubConceptLink(has, userConcept, account, "hasAccount", new Probability(1.0, 0.9))
-  val userAccountLinkedPair = ConceptLink.likConcepts(has, userConcept, account)
-
-  /**
-   * phrases
-   */
-  val userPhrase = AnnotatedPhrase("user", userConcept)
-  val customerPhrase = AnnotatedPhrase("customer", userConcept)
-  val notebookPhrase = AnnotatedPhrase("notebook", computerConcept)
-  val laptopPhrase = AnnotatedPhrase("laptop", computerConcept)
-  val desktopPhrase = AnnotatedPhrase("desktop", computerConcept)
-  val computerPhrase = AnnotatedPhrase("computer", computerConcept)
-
-
-  /**
-   * Test phrases
-   */
-  // Please install Firefox
-  val please = AnnotatedPhrase("Please")
-  val install = AnnotatedPhrase("install")
-  val fireFox = AnnotatedPhrase("FireFox")
-  val pleaseInstallFF = AnnotatedNarrative(List(please, install, fireFox), KnowledgeURI("pleaseInstallFF"))
 
   test("test Ok") {
     assert(condition = true)
