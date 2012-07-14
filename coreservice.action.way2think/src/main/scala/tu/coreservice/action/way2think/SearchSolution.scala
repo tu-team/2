730c0bf1b66a75301a52508cc5f099036599ee79
Max Talanov <max.talanov@gmail.com>
2012-07-15 00:29:54 +0400

    TU-81 - Create ThinkingLifeCycle.processSelectorRequest Dummy unit test done.

--- coreservice.action.way2think/src/main/scala/tu/coreservice/action/way2think/SearchSolution.scala
+++ coreservice.action.way2think/src/main/scala/tu/coreservice/action/way2think/SearchSolution.scala
@@ -2,13 +2,12 @@ package tu.coreservice.action.way2think
 
 import tu.model.knowledge.communication.{ContextHelper, Context}
 import tu.model.knowledge.domain.ConceptNetwork
-import tu.model.knowledge.howto.Solution
 import tu.model.knowledge.{Resource, SolvedIssue, Solutions}
 
 /**
  * @author adel chepkunov
- * Date: 10.07.12
- * Time: 7:00
+ *         Date: 10.07.12
+ *         Time: 7:00
  */
 
 class SearchSolution extends Way2Think {
@@ -34,11 +33,10 @@ object SearchSolution {
     // val outputContext = ContextHelper(res match{ case Some(x) => List[Resource](x) case None => List[Resource]() }, "OutputContex")
     // outputContext.lastResult = res
     // outputContext
-    null
+    ContextHelper(List[Resource](), this.getClass.getName + " result")
   }
 
-  def search(target: ConceptNetwork, solutions:Solutions):SolvedIssue = {
+  def search(target: ConceptNetwork, solutions: Solutions): Option[SolvedIssue] = {
     solutions.search(target, Nil)
-    null
   }
 }
