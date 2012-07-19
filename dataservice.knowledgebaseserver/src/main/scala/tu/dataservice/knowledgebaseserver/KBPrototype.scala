d8439cb4e7b7f4603c3dc0af6085f03f196d2dfe
Max Talanov <max.talanov@gmail.com>
2012-07-19 17:10:02 +0400

    Fixed coreservice

--- dataservice.knowledgebaseserver/src/main/scala/tu/dataservice/knowledgebaseserver/KBPrototype.scala
+++ dataservice.knowledgebaseserver/src/main/scala/tu/dataservice/knowledgebaseserver/KBPrototype.scala
@@ -5,6 +5,8 @@ import tu.model.knowledge.training.Goal
 import tu.model.knowledge.way2think.{JoinWay2ThinkModel, Way2ThinkModel}
 import tu.model.knowledge.action.ActionModel
 import tu.model.knowledge.critic.CriticModel
+import tu.model.knowledge.{Resource, KnowledgeURI}
+import tu.model.knowledge.annotator.AnnotatedPhrase
 
 /**
  * KBSever stub only for prototype purposes.
@@ -31,6 +33,11 @@ object KBPrototype {
             CriticModel("tu.coreservice.action.critic.analyser.ProblemDescriptionWithDesiredStateAnalyserCritic")
           ), "tu.model.knowledge.way2think.JoinWay2ThinkModel")
         ),
+      Goal("FormalizeDirectInstruction") ->
+        List[Way2ThinkModel](Way2ThinkModel("tu.coreservice.action.way2think.simulation.Simulation")),
+      Goal("FormalizeProblemDescription") ->
+        List[Way2ThinkModel](Way2ThinkModel("tu.coreservice.action.way2think.simulation.Simulation"),
+          Way2ThinkModel("tu.coreservice.action.way2think.reformulation.Reformulation")),
       Goal("GetMostProbableAction") ->
         List[Way2ThinkModel](Way2ThinkModel("tu.coreservice.action.way2think.FindMostProbableAction")
         ),
@@ -39,6 +46,34 @@ object KBPrototype {
         )
     )
 
+  def resources = goalResourceMap.values
+
+  /**
+   * Gets Map of URI -> Resource of all registered Way2ThinkModel, CriticModel, JoinWay2ThinkModel
+   * @return Map[KnowledgeURI, Resource]
+   */
+  def uriResourcesMap: Map[KnowledgeURI, Resource] = {
+    val res: Map[KnowledgeURI, Resource] = goalResourceMap.values.flatten.map {
+      r: Resource => {
+        Pair(r.uri, r)
+      }
+    }.toMap
+    res
+  }
+
+  /**
+   * Gets Map of String -> Resource of all registered Way2ThinkModel, CriticModel, JoinWay2ThinkModel
+   * @return Map[String, Resource]
+   */
+  def stringResourcesMap: Map[String, Resource] = {
+    val res: Map[String, Resource] = goalResourceMap.values.flatten.map {
+      r: Resource => {
+        Pair(r.uri.name, r)
+      }
+    }.toMap
+    res
+  }
+
   def goals = List(Goal("ProcessIncident"), Goal("ClassifyIncident"), Goal("GetMostProbableAction"), Goal("SearchSolution"))
 
   def getByGoalName(name: String): Option[List[ActionModel]] = {
@@ -46,6 +81,31 @@ object KBPrototype {
     val keys: Iterable[Goal] = resources.keys.filter {
       g: Goal => {
         g.uri.name.equals(name)
+      }
+    }
+    if (keys.size > 0) {
+      resources.get(keys.head)
+    } else {
+      None
+    }
+  }
+
+  def annotations = Map[String, AnnotatedPhrase](
+    "Please" ->
+      AnnotatedPhrase.apply("Please")
+  )
+
+  /***
+   * Gets annotations by specified word
+   * @param word to find annotations
+   * @return annotated phrase by word (for example get rid off)
+   */
+  def getAnnotationByWord(word: String): Option[AnnotatedPhrase] = {
+
+    val resources = this.annotations
+    val keys: Iterable[String] = resources.keys.filter {
+      g: String => {
+        g.equals(word)
       }
     }
     if (keys.size > 0) {
