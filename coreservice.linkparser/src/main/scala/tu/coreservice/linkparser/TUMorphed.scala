e7c5064f1d3b4038d07a067504e3aafcbc6f5b81
Max Talanov <max.talanov@gmail.com>
2012-07-26 23:10:54 +0400

    TU-82 - Integrate Way2Think Started Morphy.

--- coreservice.linkparser/src/main/scala/tu/coreservice/linkparser/TUMorphed.scala
+++ coreservice.linkparser/src/main/scala/tu/coreservice/linkparser/TUMorphed.scala
@@ -3,12 +3,15 @@ package tu.coreservice.linkparser
 import relex.morphy.{Morphed, Morphy}
 
 /**
- * @author alex
+ * @author alex toschev, max talanov
  *         Time stamp: 7/25/12 5:40 PM
  */
 
 class TUMorphed extends Morphy {
   def initialize() {}
 
-  def morph(word: String):Morphed = null
+  def morph(word: String):Morphed = {
+    val m: Morphed = new Morphed(word)
+    m
+  }
 }
