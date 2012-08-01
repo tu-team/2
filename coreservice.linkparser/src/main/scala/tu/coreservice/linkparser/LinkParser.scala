0ec6596c9443d5c3f94fbd5e6d43d96925823c33
max-talanov <max.talanov@gmail.com>
2012-08-01 06:10:50 -0700

    link parser Commit.

--- coreservice.linkparser/src/main/scala/tu/coreservice/linkparser/LinkParser.scala
+++ coreservice.linkparser/src/main/scala/tu/coreservice/linkparser/LinkParser.scala
@@ -5,6 +5,7 @@ import tu.model.knowledge.communication.{ContextHelper, Context}
 import tu.model.knowledge.Resource
 import tu.model.knowledge.annotator.AnnotatedNarrative
 import tu.coreservice.action.way2think.cry4help.Cry4HelpWay2Think
+import tu.coreservice.action.UnexpectedException
 
 /**
  * @author max talanov
@@ -24,21 +25,24 @@ class LinkParser extends Way2Think{
    */
   def apply(inputContext: Context): Context = {
 
-
+    val narrative = getLastResult(inputContext)
+    val sentences = narrative.sentences
+    val text = narrative.text
 
     val outputContext = ContextHelper(List[Resource](), this.getClass.getName)
     outputContext
   }
 
-  def getLastResult(inputContext: Context) = {
+  def getLastResult(inputContext: Context): AnnotatedNarrative = {
     try {
-      val lastResult: AnnotatedNarrative = inputContext.lastResult.asInstanceOf[AnnotatedNarrative]
-
+      val narrative: AnnotatedNarrative = inputContext.lastResult.asInstanceOf[AnnotatedNarrative]
+      narrative
     } catch {
       case e: ClassCastException => {
         val cry4Help = Cry4HelpWay2Think("$Context_lastResult_is_not_expectedType " + e.getMessage)
         // throw new UnexpectedException("$Context_lastResult_is_not_expectedType " + e.getMessage)
         ContextHelper(List[Resource](cry4Help), cry4Help, this.getClass.getName + " result")
+        throw new UnexpectedException("$Context_lastResult_is_not_expectedType " + e.getMessage)
       }
     }
   }
