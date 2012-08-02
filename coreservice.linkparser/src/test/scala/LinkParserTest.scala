6dd5861e29fbb897e552ab0b77e51860784108e0
max-talanov <max.talanov@gmail.com>
2012-08-02 05:27:52 -0700

    TU-92 Started testing LinkParser.

--- coreservice.linkparser/src/test/scala/LinkParserTest.scala
+++ coreservice.linkparser/src/test/scala/LinkParserTest.scala
@@ -1,12 +1,19 @@
 /**
  * @author max
- *        Date: 8/2/12
- *        Time: 3:52 AM
+ *         Date: 8/2/12
+ *         Time: 3:52 AM
  */
 
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.FunSuite
+import tu.coreservice.linkparser.LinkParser
+import tu.coreservice.utilities.TestDataGenerator
+import tu.model.knowledge.annotator.{AnnotatedPhrase, AnnotatedNarrative, AnnotatedSentence}
+import tu.model.knowledge.communication.{ContextHelper, Context}
+import tu.model.knowledge.frame.Frame
+import tu.model.knowledge.primitive.KnowledgeString
+import tu.model.knowledge.{KnowledgeURI, Resource}
 
 @RunWith(classOf[JUnitRunner])
 class LinkParserTest extends FunSuite {
@@ -16,7 +23,19 @@ class LinkParserTest extends FunSuite {
   }
 
   test("Lisk parser should be ok") {
-
+    val lp = new LinkParser()
+    lp.apply(createContext)
   }
+
+  private def createContext: Context = {
+    val testString = KnowledgeString("Please", "Please")
+    val phrase: AnnotatedPhrase = AnnotatedPhrase("Please", TestDataGenerator.formOfPoliteness)
+    val sentence: AnnotatedSentence = AnnotatedSentence(List(phrase))
+    val narrative: AnnotatedNarrative = new AnnotatedNarrative(List[AnnotatedSentence](sentence), KnowledgeURI("TestNarrative"))
+    val frame = Frame(Map[KnowledgeURI, Resource](narrative.uri -> narrative), KnowledgeURI("TestFrame"))
+    val context = ContextHelper(List[Resource](), frame, "TestContext")
+    context
+  }
+
 
 }
