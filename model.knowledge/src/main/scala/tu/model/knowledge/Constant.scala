e5a9066c156abda58c3b0a25ed0ab0cd0aca45f1
Max Talanov <max.talanov@gmail.com>
2012-08-29 01:44:44 +0400

    TU-130 - create test suite Corrected: Concept, AnnotatedSentence, AnnotatedPhrase, Constant.

--- model.knowledge/src/main/scala/tu/model/knowledge/Constant.scala
+++ model.knowledge/src/main/scala/tu/model/knowledge/Constant.scala
@@ -37,6 +37,7 @@ object Constant {
   val SPECIALISATION_LINK_NAME = "specialisationLink"
   val PHRASES_LINK_NAME = "phrasesLink"
   val SENTENCES_LINK_NAME = "sentencesLink"
+  val CONCEPT_LINK_NAME = "conceptLink"
   val CONCEPT_LINK_SOURCE_NAME = "conceptLinkSrc"
   val CONCEPT_LINK_DESTINATION_NAME = "conceptLinkDst"
   val NODES_LINK_NAME = "nodesLink"
