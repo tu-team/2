4d74b8987219f6b5fb01d96d877bc2090674d573
Max Talanov <max.talanov@gmail.com>
2012-09-29 01:13:37 +0400

    TU-150 - KBAdapter.solutions method Coded.

--- dataservice.knowledgebaseserver/src/main/scala/tu/dataservice/knowledgebaseserver/KBAdapter.scala
+++ dataservice.knowledgebaseserver/src/main/scala/tu/dataservice/knowledgebaseserver/KBAdapter.scala
@@ -6,9 +6,10 @@ import tu.model.knowledge.way2think.{JoinWay2ThinkModel, Way2ThinkModel}
 import tu.model.knowledge.action.ActionModel
 import tu.model.knowledge.critic.CriticModel
 import tu.model.knowledge._
-import domain.{ConceptLink, ConceptNetwork, Concept}
+import domain.{ConceptTag, ConceptLink, ConceptNetwork, Concept}
 import annotator.AnnotatedPhrase
-import tu.model.knowledge.howto.Solution
+import frame.Frame
+import howto.{HowTo, Solution}
 
 /**
  * KBSever stub only for prototype purposes.
@@ -23,7 +24,7 @@ object KBAdapter {
   val goalsName = "goals_name"
   val domainName = "domain_name"
 
-  val selfReflectiveCritics="selfReflectiveCritics"
+  val selfReflectiveCritics = "selfReflectiveCritics"
 
   var kb = N4JKB
 
@@ -149,26 +150,8 @@ object KBAdapter {
   }
 
   def getDefaultDomain: Pair[ConceptNetwork, List[AnnotatedPhrase]] = {
-    val CONCEPT = Concept("concept")
-    val CONCEPT_LINK = ConceptLink(CONCEPT, CONCEPT, "conceptLink")
-    val conceptPhrase = AnnotatedPhrase("concept", CONCEPT)
-    val wordConcept = Concept.createSubConcept(CONCEPT, "word")
-    val wordPhrase = AnnotatedPhrase("word", CONCEPT)
-    val subjectConcept = Concept.createSubConcept(CONCEPT, "subject")
-    val subjectPhrase = AnnotatedPhrase("subject", CONCEPT)
-    val objectConcept = Concept.createSubConcept(CONCEPT, "object")
-    val objectPhrase = AnnotatedPhrase("object", CONCEPT)
-    val has = ConceptLink.createSubConceptLink(CONCEPT_LINK, subjectConcept, objectConcept, "has", new Probability(1.0, 1.0))
-    val hasPhrase = AnnotatedPhrase("has", has)
-    val isLink = ConceptLink.createSubConceptLink(CONCEPT_LINK, subjectConcept, objectConcept, "is")
-    val isPhrase = AnnotatedPhrase("is", isLink)
-    val generalisationLink = ConceptLink.createSubConceptLink(CONCEPT_LINK, subjectConcept, objectConcept, "generalisation")
-    val isAPhrase = AnnotatedPhrase("is a", generalisationLink)
-    val kindOfPhrase = AnnotatedPhrase("kind of", generalisationLink)
-    val concepts = List[Concept](CONCEPT, wordConcept, subjectConcept, objectConcept)
-    val conceptLinks: List[ConceptLink] = List(CONCEPT_LINK, has, isLink, generalisationLink)
-    val phrases: List[AnnotatedPhrase] = List(conceptPhrase, wordPhrase, subjectPhrase, objectPhrase, hasPhrase, isPhrase, isAPhrase, kindOfPhrase)
-    (ConceptNetwork(concepts, conceptLinks, KnowledgeURI("domainModel")), phrases)
+
+    (ConceptNetwork(Defaults.concepts, Defaults.conceptLinks, KnowledgeURI("domainModel")), Defaults.phrases)
   }
 
   def getDefaultDomainConceptNetwork: ConceptNetwork = {
@@ -180,9 +163,7 @@ object KBAdapter {
   }
 
   def solutions(): List[SolvedIssue] = {
-
     val res: List[SolvedIssue] = kb.loadChildrenList(solutionsName).map(x => SolvedIssue.load(kb, x))
-
     if (res.isEmpty) {
       //save solutions
       getDefaultSolutions
@@ -199,19 +180,19 @@ object KBAdapter {
     val in_uri = new KnowledgeURI("namespace", "name", "revision")
     def getTestSolvedIssue2: SolvedIssue = {
 
-      val s = new Solution(List(TestDataGenerator.generateReinstallIE8HowTo), in_uri)
-      new SolvedIssue(TestDataGenerator.iHaveProblemWithIE8Simulation, s, in_uri, probability)
+      val s = new Solution(List(Defaults.generateReinstallIE8HowTo), in_uri)
+      new SolvedIssue(Defaults.iHaveProblemWithIE8Simulation, s, in_uri, probability)
     }
 
     def getTestSolvedIssue3: SolvedIssue = {
 
-      val s = new Solution(List(TestDataGenerator.generateReinstallIE8HowTo), in_uri)
-      new SolvedIssue(TestDataGenerator.iHaveProblemWithIE8Reformulation, s, in_uri, probability)
+      val s = new Solution(List(Defaults.generateReinstallIE8HowTo), in_uri)
+      new SolvedIssue(Defaults.iHaveProblemWithIE8Reformulation, s, in_uri, probability)
     }
 
     val uri = new KnowledgeURI("namespace", "name", "revision")
 
-    val s = new Solution(List(TestDataGenerator.generateInstallFirefoxHowTo), uri)
+    val s = new Solution(List(Defaults.generateInstallFirefoxHowTo), uri)
     List(new SolvedIssue(TestDataGenerator.pleaseInstallFFSimulation, s, uri, new Probability), getTestSolvedIssue2, getTestSolvedIssue3)
   }
 
@@ -236,9 +217,105 @@ object KBAdapter {
   }
 
   def getReflectiveCritics: List[CriticModel] = {
-   // kb.loadChildrenList(selfReflectiveCritics).map(x => CriticModel.load(kb, x))
+    // kb.loadChildrenList(selfReflectiveCritics).map(x => CriticModel.load(kb, x))
     List[CriticModel](CriticModel("tu.coreservice.action.critic.manager.DoNotUnderstandManager")
     )
+  }
+
+  object Defaults {
+
+    val CONCEPT = Concept("concept")
+    val CONCEPT_LINK = ConceptLink(CONCEPT, CONCEPT, "conceptLink")
+    val conceptPhrase = AnnotatedPhrase("concept", CONCEPT)
+    val wordConcept = Concept.createSubConcept(CONCEPT, "word")
+    val wordPhrase = AnnotatedPhrase("word", CONCEPT)
+    val subjectConcept = Concept.createSubConcept(CONCEPT, "subject")
+    val subjectPhrase = AnnotatedPhrase("subject", CONCEPT)
+    val objectConcept = Concept.createSubConcept(CONCEPT, "object")
+    val objectPhrase = AnnotatedPhrase("object", CONCEPT)
+    val has = ConceptLink.createSubConceptLink(CONCEPT_LINK, subjectConcept, objectConcept, "has", new Probability(1.0, 1.0))
+    val hasPhrase = AnnotatedPhrase("has", has)
+    val isLink = ConceptLink.createSubConceptLink(CONCEPT_LINK, subjectConcept, objectConcept, "is")
+    val isPhrase = AnnotatedPhrase("is", isLink)
+    val generalisationLink = ConceptLink.createSubConceptLink(CONCEPT_LINK, subjectConcept, objectConcept, "generalisation")
+    val isAPhrase = AnnotatedPhrase("is a", generalisationLink)
+    val kindOfPhrase = AnnotatedPhrase("kind of", generalisationLink)
+    val concepts = List[Concept](CONCEPT, wordConcept, subjectConcept, objectConcept)
+    val conceptLinks: List[ConceptLink] = List(CONCEPT_LINK, has, isLink, generalisationLink)
+    val phrases: List[AnnotatedPhrase] = List(conceptPhrase, wordPhrase, subjectPhrase, objectPhrase, hasPhrase, isPhrase, isAPhrase, kindOfPhrase)
+
+    val softwareConcept = Concept.createSubConcept(objectConcept, "sofware")
+    val browserConcept = Concept.createSubConcept(softwareConcept, "Browser")
+    val internetExplorerConcept = Concept.createSubConcept(browserConcept, "Microsoft Internet Explorer")
+    val versionConcept = Concept.createSubConcept(objectConcept, "version")
+    val userConcept = Concept.createSubConcept(subjectConcept, "user")
+    val addressConcept = Concept.createSubConcept(objectConcept, "address")
+    val computerConcept = Concept.createSubConcept(objectConcept, "computer")
+    val firefoxConcept = Concept.createSubConcept(browserConcept, "Mozilla Firefox")
+    val systemConcept = Concept.createSubConcept(objectConcept, "system")
+
+    /**
+     * Links
+     */
+    val missLink = ConceptLink(userConcept, objectConcept, "miss")
+    val hasNo = ConceptLink(subjectConcept, objectConcept, "hasNo")
+    val appliedLink = ConceptLink(subjectConcept, objectConcept, "applied")
+
+    /**
+     * HowTo-s
+     */
+    val installHowTo = new HowTo(List[Frame](Frame(CONCEPT)), List[ConceptTag](), KnowledgeURI("installHowTo"))
+    val reinstallHowTo = new HowTo(List[Frame](Frame(CONCEPT)), List[ConceptTag](), KnowledgeURI("reinstallHowTo"))
+
+    // actions
+    val actionConcept = Concept("action")
+    val installConcept = Concept.createSubConcept(actionConcept, "install")
+    val removeConcept = Concept.createSubConcept(actionConcept, "remove")
+    val cleanConcept = Concept.createSubConcept(actionConcept, "clean")
+
+
+    /**
+     * Generates reinstall IE8 HowTo
+     * @return HowTo
+     */
+    def generateReinstallIE8HowTo = reinstallIEHowTo
+
+    /**
+     * Generates install Firefox HowTo
+     * @return  HowTo
+     */
+    def generateInstallFirefoxHowTo = installFirefoxHowTo
+
+    val installFirefoxHowTo = HowTo.createInstance(installHowTo, List(Frame(firefoxConcept)))
+    val reinstallIEHowTo = HowTo.createInstance(installHowTo, List(Frame(internetExplorerConcept)))
+
+    // User miss Internet Explorer 8 simulated
+    val userInst = Concept.createInstanceConcept(userConcept)
+    val internetExplorerInst = Concept.createInstanceConcept(internetExplorerConcept)
+    val versionInst = Concept.createInstanceConcept(versionConcept, "8")
+    val userMissInternetExplorer = ConceptLink.createInstanceConceptLink(missLink, userInst, internetExplorerInst)
+    val internetExplorerHasVersion = ConceptLink.createInstanceConceptLink(has, internetExplorerInst, versionInst)
+    val iHaveProblemWithIE8Simulation = new ConceptNetwork(List[Concept](userInst, internetExplorerInst, versionInst),
+      List[ConceptLink](userMissInternetExplorer, internetExplorerHasVersion), KnowledgeURI("iHaveProblemWithIE8Simulation"))
+
+    // User miss Internet Explorer 8 reformulated
+    val userInstRef = Concept.createInstanceConcept(userConcept)
+    val computerInstRef = Concept.createInstanceConcept(computerConcept)
+    val userHasComputerInst = ConceptLink.createInstanceConceptLink(has, userInstRef, computerInstRef)
+    val addressInstRef = Concept.createInstanceConcept(addressConcept, "someAddress")
+    val computerHasAddressRef = ConceptLink.createInstanceConceptLink(has, computerInstRef, addressInstRef)
+    val internetExplorerInstRef = Concept.createInstanceConcept(internetExplorerConcept)
+    val computerHasNoInternetExplorerInstRef = ConceptLink.createInstanceConceptLink(hasNo, computerInstRef, internetExplorerInstRef)
+    val internetExplorerHasVersionInstRef = ConceptLink.createInstanceConceptLink(has, internetExplorerInstRef, versionInst)
+    val iHaveProblemWithIE8Reformulation = new ConceptNetwork(List[Concept](userInstRef, computerInstRef, addressInstRef, internetExplorerInstRef),
+      List[ConceptLink](userHasComputerInst, computerHasAddressRef, computerHasNoInternetExplorerInstRef, internetExplorerHasVersionInstRef), KnowledgeURI("iHaveProblemWithIE8Reformulation"))
+
+    // Please install Firefox simulation
+    val installActionInst = Concept.createInstanceConcept(installConcept)
+    val firefoxConceptInst = Concept.createInstanceConcept(firefoxConcept)
+    val systemInstallFirefox = ConceptLink.createSubConceptLink(appliedLink, systemConcept, firefoxConceptInst, "systemInstallFirefox")
+    val pleaseInstallFFSimulation = new ConceptNetwork(List[Concept](installActionInst, firefoxConceptInst), List[ConceptLink](systemInstallFirefox),
+      KnowledgeURI("pleaseInstallFFSimulation"))
   }
 
 }
--- model.knowledge/src/main/scala/tu/model/knowledge/SolvedIssue.scala
+++ model.knowledge/src/main/scala/tu/model/knowledge/SolvedIssue.scala
@@ -1,38 +1,31 @@
 package tu.model.knowledge
 
-import annotator.AnnotatedWord
-import domain.{Concept, ConceptNetwork}
-import howto.{Solution, HowTo}
-import semanticnetwork.SemanticNetwork
+import domain.ConceptNetwork
+import howto.Solution
 import tu.model.knowledge.KBMap._
 import tu.exception.UnexpectedException
 import scala._
 import tu.model.knowledge.helper.URIGenerator
 
 /**
- * Created by IntelliJ IDEA.
- * User: adel
- * Date: 10.07.12
- * Time: 7:37
- * To change this template use File | Settings | File Templates.
+ * @author adel chepkunov
+ *         Date: 10.07.12
+ *         Time: 7:37
  */
 
-case class SolvedIssue (var issue:ConceptNetwork, val solution: Solution,   _uri: KnowledgeURI, _probability: Probability)
-                                                                              extends Resource(_uri, _probability) {
-  override def save(kb:KB, parent:KBNodeId, key:String, linkType:String, saved: List[String] = Nil): Boolean = {
+case class SolvedIssue(var issue: ConceptNetwork, solution: Solution, _uri: KnowledgeURI, _probability: Probability)
+  extends Resource(_uri, _probability) {
+  override def save(kb: KB, parent: KBNodeId, key: String, linkType: String, saved: List[String] = Nil): Boolean = {
     var res = kb.saveResource(this, parent, key, linkType)
-
     issue.save(kb, this, "", Constant.CONCEPTS_NETWORK_NAME)
     solution.save(kb, this, "", Constant.SOLUTION_NAME)
   }
 }
 
-
 object SolvedIssue {
-  def apply(issue:ConceptNetwork,solution:Solution): SolvedIssue = {
-    new SolvedIssue(issue,solution, URIGenerator.generateURI(SolvedIssue.getClass.getName), new Probability())
+  def apply(issue: ConceptNetwork, solution: Solution): SolvedIssue = {
+    new SolvedIssue(issue, solution, URIGenerator.generateURI(SolvedIssue.getClass.getName), new Probability())
   }
-
 
   def load(kb: KB, parentId: KBNodeId, key: String, linkType: String): SolvedIssue = {
     val selfMap = kb.loadChild(parentId, key, linkType)
@@ -43,9 +36,9 @@ object SolvedIssue {
     load(kb, selfMap)
   }
 
-  def load(kb: KB, selfMap: Map[String,  String]): SolvedIssue = {
+  def load(kb: KB, selfMap: Map[String, String]): SolvedIssue = {
     val ID = new KBNodeId(selfMap)
-    val issue:ConceptNetwork = ConceptNetwork.load(kb, ID, "", Constant.CONCEPTS_NETWORK_NAME)
+    val issue: ConceptNetwork = ConceptNetwork.load(kb, ID, "", Constant.CONCEPTS_NETWORK_NAME)
     val solution: Solution = Solution.load(kb, ID, "", Constant.SOLUTION_NAME)
     new SolvedIssue(issue, solution, new KnowledgeURI(selfMap), new Probability(selfMap))
   }
