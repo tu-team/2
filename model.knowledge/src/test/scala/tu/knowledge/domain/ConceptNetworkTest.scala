7e999bc22900d469deddbca63a9ddc2cc1bb6a56
Adel Chepkunov <achepkunov@yandex.ru>
2012-10-12 12:49:16 +0400

    restore diff with toText

--- model.knowledge/src/test/scala/tu/knowledge/domain/ConceptNetworkTest.scala
+++ model.knowledge/src/test/scala/tu/knowledge/domain/ConceptNetworkTest.scala
@@ -9,35 +9,49 @@ package tu.knowledge.domain
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.FunSuite
-import tu.model.knowledge.{Resource, TypedKLine, Probability, KnowledgeURI}
 import tu.model.knowledge.domain.{ConceptNetwork, ConceptLink, Concept}
 import tu.model.knowledge.annotator.AnnotatedPhrase
 import tu.model.knowledge.primitive.KnowledgeString
+import tu.model.knowledge._
 
 @RunWith(classOf[JUnitRunner])
 class ConceptNetworkTest extends FunSuite {
 
   val namespace = "testNamespace"
-  val name = "name"
+  val name = "CONCEPT"
   val revision = "rev"
   val uri = new KnowledgeURI(namespace, name, revision)
   val probability = new Probability
   val sourceContent = "Source"
   val destinationContent = "Dest"
-  val content = new KnowledgeString("name", uri)
+  val content = new KnowledgeString(name, uri)
   val concept = new Concept(TypedKLine[Concept]("generalisations"), TypedKLine[Concept]("specialisations"),
     TypedKLine[AnnotatedPhrase]("user", AnnotatedPhrase("user")), content, List[ConceptLink](), uri)
-  val subject = Concept("subject")
-  val objectConcept = Concept("object")
-  val has = ConceptLink(subject, objectConcept, "has")
+  val subjectConcept = Concept.createSubConcept(concept, "subject")
+  val objectConcept = Concept.createSubConcept(concept, "object")
+  val has = ConceptLink(subjectConcept, objectConcept, "has")
 
   test("test Ok") {
     assert(condition = true)
   }
 
   test("ConceptNetwork should contain concepts and their contents") {
-    val sn0 = new ConceptNetwork(List[Concept](concept), List(has),uri)
-    expect(sn0.nodes(0).content.toString)(content.toString)
+    val sn0 = new ConceptNetwork(List[Concept](concept), List(has), uri)
+    expect(content.toString)(sn0.nodes(0).content.toString)
+  }
+
+  test("ConceptNetwork should be exported via toText") {
+    val sn0 = new ConceptNetwork(List[Concept](concept, subjectConcept), List(), uri)
+    val res0 = sn0.toText
+    expect("subject <- CONCEPT")(res0)
+
+    val sn1 = new ConceptNetwork(List[Concept](concept, subjectConcept, objectConcept), List(), uri)
+    val res1 = sn1.toText
+    expect("subject <- CONCEPT\nobject <- CONCEPT")(res1)
+
+    val sn2 = new ConceptNetwork(List[Concept](concept, subjectConcept, objectConcept), List(has), uri)
+    val res2 = sn2.toText
+    expect("subject[<has> objectConcept] <- CONCEPT\nobject[subjectConcept <has>] <- CONCEPT")(res2)
   }
 
 }
