cac263bb51695fa211c990d34f0a8aa6323000b4
max-talanov <max.talanov@gmail.com>
2012-10-23 02:54:47 -0700

    tu-175 Started LongTermMemory

--- dataservice.knowledgebaseserver/src/test/scala/tu/dataservice/memory/LongTermMemoryTest.scala
+++ dataservice.knowledgebaseserver/src/test/scala/tu/dataservice/memory/LongTermMemoryTest.scala
@@ -9,12 +9,38 @@ package tu.dataservice.memory
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.FunSuite
+import tu.dataservice.knowledgebaseserver.KBAdapter
+import tu.model.knowledge.domain.Concept
+import tu.model.knowledge.KnowledgeURI
 
 @RunWith(classOf[JUnitRunner])
 class LongTermMemoryTest extends FunSuite {
 
+  val testURIString = "test"
+  val testURI = KnowledgeURI(testURIString)
+
   test("test Ok") {
     assert(true)
+  }
+
+  test("merge concepts") {
+    val cp = Concept("parent")
+    val cc = Concept.createSubConcept(cp, "child")
+    val dm = KBAdapter.domainModel(testURIString)
+    assert(dm.nodes.size > 0)
+    val c = dm.nodes(0)
+    val stmc = new ShortTermMemoryConceptWrapper(cc, c)
+    val resCN = LongTermMemory.merge(stmc, testURI)
+    /* val children = resCN.nodes.filter {
+      c: Concept => {
+        c.generalisations.frames.filter{
+          val keyValue = {
+
+          }
+        }
+      }
+    }*/
+    // assert (children.size > 0)
   }
 
 }
