c0827dc87fdaac2b7ac4f83beb38c8ca92dc280f
Max Talanov <max.talanov@gmail.com>
2012-12-03 00:18:38 +0400

    TU-205 - Fix OpenNLP dependency for RelEX. Fixed nlp.server dependence.

--- nlp.server/src/main/scala/NLPFactory.scala
+++ nlp.server/src/main/scala/NLPFactory.scala
@@ -3,13 +3,13 @@ package tu.nlp.server
 
 /**
  *
- * @author: Alexander Toschev
- *          Date: 12/1/12
- *          Time: 1:46 PM
+ * @author Alexander Toschev
+ *         Date: 12/1/12
+ *         Time: 1:46 PM
  *
  */
 object NLPFactory {
   def createProcessor(): NLPProcessor = {
-    return RelexNLPProcessor
+    RelexNLPProcessor
   }
 }
