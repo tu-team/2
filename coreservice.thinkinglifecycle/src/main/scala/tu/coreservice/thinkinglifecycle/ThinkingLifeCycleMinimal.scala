386b8131c79480822e3e983d9a8b0ed2ff7b1cdc
Max Talanov <max.talanov@gmail.com>
2013-01-07 00:57:05 +0400

    TU-225 - Fix, Cry4Help behavior, as it uses ReflectionLastResult.

--- coreservice.thinkinglifecycle/src/main/scala/tu/coreservice/thinkinglifecycle/ThinkingLifeCycleMinimal.scala
+++ coreservice.thinkinglifecycle/src/main/scala/tu/coreservice/thinkinglifecycle/ThinkingLifeCycleMinimal.scala
@@ -124,6 +124,7 @@ class ThinkingLifeCycleMinimal
         }
         case None => //End
       }
+
       goalManager.nextGoal(globalContext)
     }
     log debug "apply()"
@@ -149,7 +150,7 @@ class ThinkingLifeCycleMinimal
    * @return List of Contexts results of processing.
    */
   def processResources(resources: List[Resource], reflectiveFlag: Boolean = false): List[ShortTermMemory] = {
-    log debug "processResources(" + resources + ": List[Resource]): List[ShortTermMemory]"
+    log info "processResources(" + resources + ": List[Resource] " + reflectiveFlag + " Boolean ): List[ShortTermMemory]"
     val contexts: List[List[ShortTermMemory]] = for (r <- resources) yield {
       val resContext = translate(r, this.globalContext)
       if (resContext != null) {
