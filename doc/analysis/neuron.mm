d06a5d5a8fce836c5e6e9b638826d8084fcfd82f
Max Talanov <max.talanov@gmail.com>
2014-02-27 17:42:32 +0400

    updated neuron mind map

--- doc/analysis/neuron.mm
+++ doc/analysis/neuron.mm
@@ -5,16 +5,50 @@
 <node CREATED="1393486446075" ID="ID_1398560650" MODIFIED="1393486449007" TEXT="axon terminal"/>
 <node CREATED="1393486455979" ID="ID_926622359" MODIFIED="1393486471545" TEXT="myelin sheath"/>
 <node CREATED="1393486472748" ID="ID_444293619" MODIFIED="1393486488897" TEXT="node of Ranvier"/>
-<node CREATED="1393486796839" ID="ID_1661120941" MODIFIED="1393486800628" TEXT="axon hillock"/>
+<node CREATED="1393486796839" ID="ID_1661120941" MODIFIED="1393486800628" TEXT="axon hillock">
+<node CREATED="1393497246509" ID="ID_874494767" MODIFIED="1393497252921" TEXT="greatest density of voltage-dependent sodium channels"/>
+</node>
 </node>
 <node CREATED="1393486436693" ID="ID_257412556" MODIFIED="1393486440424" POSITION="right" TEXT="soma"/>
-<node CREATED="1393485380203" ID="ID_1126994319" MODIFIED="1393485391281" POSITION="right" TEXT="dendrite"/>
+<node CREATED="1393485380203" ID="ID_1126994319" MODIFIED="1393485391281" POSITION="right" TEXT="dendrite">
+<node CREATED="1393496905751" ID="ID_780203914" MODIFIED="1393496910956" TEXT="dendrite spine"/>
+</node>
 <node CREATED="1393487716985" ID="ID_1498194830" MODIFIED="1393487720238" POSITION="left" TEXT="membrane">
 <node CREATED="1393487733977" ID="ID_264580619" MODIFIED="1393487737757" TEXT="ion pumps"/>
 <node CREATED="1393491964270" ID="ID_961910246" MODIFIED="1393492317237" TEXT="ion channels">
 <node CREATED="1393491971607" ID="ID_61076368" MODIFIED="1393492394353" TEXT="chemically gated: permeable for some types of ions"/>
 <node CREATED="1393491990856" ID="ID_825539395" MODIFIED="1393492000013" TEXT="voltage gated"/>
 </node>
+</node>
+<node CREATED="1393499896543" ID="ID_1329248683" MODIFIED="1393499898093" POSITION="left" TEXT="brain">
+<node CREATED="1393499900008" ID="ID_889084444" MODIFIED="1393499926093" TEXT="cerebral cortex: 16.3 billion neurons"/>
+<node CREATED="1393499928433" ID="ID_603336075" MODIFIED="1393500057669" TEXT="cerebellum: 69 billion neurons"/>
+<node CREATED="1393500062173" ID="ID_1363639993" MODIFIED="1393500070002" TEXT="neurogenesis">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <span style="word-spacing: 0px; display: inline !important; font-style: normal; font-weight: normal; color: rgb(0, 0, 0); float: none; text-align: start; line-height: 19.200000762939453px; font-size: 12.727272033691406px; letter-spacing: normal; background-color: rgb(255, 255, 255); font-variant: normal; font-family: sans-serif; white-space: normal; text-indent: 0px; text-transform: none"><font face="sans-serif" size="12.727272033691406px" color="rgb(0, 0, 0)">However, more recent studies of the age of human neurons suggest that this process occurs only for a minority of cells, and the overwhelming majority of neurons comprising the</font></span><font face="sans-serif" size="12.727272033691406px" color="rgb(0, 0, 0)"><span class="Apple-converted-space">&#160;</span></font><a style="word-spacing: 0px; font-style: normal; background-repeat: repeat; color: rgb(11, 0, 128); font-weight: normal; background-image: none; text-decoration: none; text-align: start; line-height: 19.200000762939453px; font-size: 12.727272033691406px; letter-spacing: normal; background-color: rgb(255, 255, 255); background-position: initial initial; font-variant: normal; font-family: sans-serif; white-space: normal; text-indent: 0px; text-transform: none" href="https://en.wikipedia.org/wiki/Neocortex" title="Neocortex"><font face="sans-serif" size="12.727272033691406px" color="rgb(11, 0, 128)">neocortex</font></a><span class="Apple-converted-space"><font face="sans-serif" size="12.727272033691406px" color="rgb(0, 0, 0)">&#160;</font></span><font face="sans-serif" size="12.727272033691406px" color="rgb(0, 0, 0)"><span style="display: inline !important; word-spacing: 0px; font-style: normal; color: rgb(0, 0, 0); font-weight: normal; float: none; text-align: start; line-height: 19.200000762939453px; font-size: 12.727272033691406px; letter-spacing: normal; background-color: rgb(255, 255, 255); font-variant: normal; font-family: sans-serif; white-space: normal; text-indent: 0px; text-transform: none">were formed before birth and persist without replacement.</span></font><font face="sans-serif" color="rgb(11, 0, 128)"><span><sup style="word-spacing: 0px; font-style: normal; color: rgb(0, 0, 0); font-weight: normal; text-align: start; line-height: 1em; letter-spacing: normal; background-color: rgb(255, 255, 255); font-variant: normal; font-family: sans-serif; white-space: normal; text-indent: 0px; text-transform: none" class="reference" id="cite_ref-r1_2-1"><a style="background-repeat: repeat; color: rgb(11, 0, 128); background-image: none; text-decoration: none; background-position: initial initial; white-space: nowrap" href="https://en.wikipedia.org/wiki/Neuron#cite_note-r1-2">[</a></sup></span><sup style="word-spacing: 0px; font-style: normal; color: rgb(0, 0, 0); font-weight: normal; text-align: start; line-height: 1em; letter-spacing: normal; background-color: rgb(255, 255, 255); font-variant: normal; font-family: sans-serif; white-space: normal; text-indent: 0px; text-transform: none" class="reference" id="cite_ref-r1_2-1"><a style="background-repeat: repeat; color: rgb(11, 0, 128); background-image: none; text-decoration: none; background-position: initial initial; white-space: nowrap" href="https://en.wikipedia.org/wiki/Neuron#cite_note-r1-2">2<span>]</span></a></sup></font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1393501129623" ID="ID_1841000527" MODIFIED="1393501133259" POSITION="left" TEXT="classification">
+<node CREATED="1393501135038" ID="ID_1486765253" MODIFIED="1393501683114" TEXT="polarity">
+<node CREATED="1393501687166" ID="ID_787899971" MODIFIED="1393501690946" TEXT="unipolar"/>
+<node CREATED="1393501706686" ID="ID_319725908" MODIFIED="1393501715835" TEXT="pseudounipolar"/>
+<node CREATED="1393501691878" ID="ID_67591797" MODIFIED="1393501696498" TEXT="bipolar"/>
+<node CREATED="1393501697093" ID="ID_167752540" MODIFIED="1393501702746" TEXT="multipolar">
+<node CREATED="1393501884158" ID="ID_1794748474" MODIFIED="1393501990631" TEXT="Golgi I: long projecting axonal process; pyramidal "/>
+<node CREATED="1393501998507" ID="ID_272659550" MODIFIED="1393502351358" TEXT="Golgi II: axonal process projects locally; granule cell"/>
+</node>
+</node>
+<node CREATED="1393501304269" ID="ID_195145719" MODIFIED="1393501330547" TEXT="functional"/>
+<node CREATED="1393501345974" ID="ID_550111303" MODIFIED="1393501351676" TEXT="discharge patterns"/>
 </node>
 </node>
 </map>
