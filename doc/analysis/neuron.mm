6fb2eb47eeb1feaff0af6e2de34bcef7ae4baca3
Max Talanov <max.talanov@gmail.com>
2014-02-27 11:32:01 +0400

    started neuron mind map and complete neuron structures analysis classification mind map

---
+++ doc/analysis/neuron.mm
@@ -0,0 +1,7 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1393485269965" ID="ID_1137201785" MODIFIED="1393485320703" TEXT="neuron">
+<node CREATED="1393485348016" ID="ID_91051987" MODIFIED="1393485377238" POSITION="right" TEXT="axon"/>
+<node CREATED="1393485380203" ID="ID_1126994319" MODIFIED="1393485391281" POSITION="right" TEXT="dendrite"/>
+</node>
+</map>
