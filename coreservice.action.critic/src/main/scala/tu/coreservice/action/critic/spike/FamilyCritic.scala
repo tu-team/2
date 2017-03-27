eb49355c138268bcbfa7a4977c042532aa03a457
Adramalek <beherith10.02@gmail.com>
2017-03-27 14:46:34 +0300

    Refactor critic.

--- coreservice.action.critic/src/main/scala/tu/coreservice/action/critic/spike/FamilyCritic.scala
+++ coreservice.action.critic/src/main/scala/tu/coreservice/action/critic/spike/FamilyCritic.scala
@@ -1,28 +1,28 @@
 package tu.coreservice.action.critic.spike
 
 import tu.coreservice.action.critic.{Critic, CriticLink}
-import tu.coreservice.action.way2think.spike.SpikeGeneratorWay2Think
+import tu.model.knowledge.Constant.SPIKE_RESOURCE
 import tu.model.knowledge.communication.ShortTermMemory
-import tu.model.knowledge.{KnowledgeURI, Probability}
-import tu.model.knowledge.Constant.SPIKE_FILES_STORAGE_DIRECTORY
+import tu.model.knowledge.neugogar.{RoboticData, RoboticDataContainer, SpikeDataContainer}
+import tu.model.knowledge.{Constant, KnowledgeURI, Probability}
+import tu.model.knowledge.selector.SelectorRequest
 
 abstract class FamilyCritic(_excluded: List[CriticLink], _included: List[CriticLink], _uri: KnowledgeURI, _probability: Probability)
   extends Critic(_excluded, _included, _uri, _probability) {
-
-  private var _storageDirectory: String = SPIKE_FILES_STORAGE_DIRECTORY
-
-  def storageDirectory = _storageDirectory
-  def storageDirectory_= (newDir: Option[String]) = {
-    newDir match {
-      case None => _storageDirectory = SPIKE_FILES_STORAGE_DIRECTORY
-      case Some(dir) => _storageDirectory = dir
-    }
-  }
 
   override def start(): Boolean = false
   override def stop(): Boolean = false
 
   protected def apply(inputContext: ShortTermMemory, family: String): ShortTermMemory = {
-    SpikeGeneratorWay2Think(_storageDirectory,family).apply(inputContext)
+    inputContext.findByName(SPIKE_RESOURCE) match {
+      case Some(resource: RoboticDataContainer) =>
+        val timeList = resource._data.toStream.map((rd: RoboticData) => rd._time).toList
+        val spikeData = new SpikeDataContainer(family,timeList)
+        inputContext._frames += KnowledgeURI(SPIKE_RESOURCE) -> spikeData
+        inputContext.lastResult = Some(SelectorRequest(List(KnowledgeURI(Constant.SELECTOR_REQUEST_SPIKE_GENERATOR_URI)),
+                                        KnowledgeURI(Constant.SELECTOR_REQUEST_SPIKE_GENERATOR_URI_NAME)))
+        inputContext
+      case _:Any => inputContext
+    }
   }
 }
