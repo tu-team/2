67a81b723b07c606f285251bc9c41992c046407d
Daniil Polyakh <p.daniil@live.com>
2017-05-08 15:39:48 +0300

    TU-App: added structured project with MVVM support.

---
+++ windows.app.ui/TU/TU.App/App.xaml.cs
@@ -0,0 +1,122 @@
+﻿namespace TU.App
+{
+    using System;
+    using System.Collections.Generic;
+
+    using Windows.ApplicationModel.Activation;
+    using Windows.UI.Core;
+
+    using Caliburn.Micro;
+
+    using ViewModels;
+
+    /// <summary>
+    /// Provides Caliburn.Micro framework behavior of Application class
+    /// </summary>
+    sealed partial class App
+    {
+        #region Private Members
+
+        /// <summary>
+        /// Application IoC container
+        /// </summary>
+        private WinRTContainer _container;
+
+        #endregion
+
+        /// <summary>
+        /// Constructor 
+        /// Used for for setup Caliburn.Micro framework
+        /// </summary>
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        #region Overrides of CaliburnApplication
+
+        /// <summary>
+        /// Overrides of Configure method from <see cref="CaliburnApplication"/>
+        /// </summary>
+        protected override void Configure()
+        {
+            _container = new WinRTContainer();
+            _container.RegisterWinRTServices();
+
+            _container
+                .Singleton<ShellPageViewModel>();
+
+            ConfigureNavigation();
+        }
+
+        /// <summary>
+        /// Configuring application navigation service
+        /// </summary>
+        private void ConfigureNavigation()
+        {
+            var navigation = SystemNavigationManager.GetForCurrentView();
+            navigation.AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
+           
+            navigation.BackRequested += (o, e) =>
+            {
+                e.Handled = true;
+
+                var navigationService = IoC.Get<INavigationService>();
+                if (navigationService.CanGoBack)
+                    navigationService.GoBack();
+            };
+        }
+
+        #endregion
+
+        #region Overrides of application life cycle
+
+        /// <summary>
+        /// Handling OnLaunched event of application life cycle
+        /// </summary>
+        /// <param name="args"></param>
+        protected override void OnLaunched(LaunchActivatedEventArgs args)
+        {
+            if (args.PreviousExecutionState == ApplicationExecutionState.Running)
+                return;
+
+            DisplayRootViewFor<ShellPageViewModel>();
+        }
+
+        #endregion
+
+        #region Overrides of IoC
+
+        /// <summary>
+        /// Overrides of GetInstance method from <see cref="IoC"/> class to work with application container
+        /// </summary>
+        /// <param name="service">Type of getting instance</param>
+        /// <param name="key">Key of getting instance</param>
+        /// <returns>Instance from application container</returns>
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        /// <summary>
+        /// Overrides of GetAllInstances method from <see cref="IoC"/> class to work with application container
+        /// </summary>
+        /// <param name="service">Type of getting instances</param>
+        /// <returns>List of instances from application container</returns>
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        /// <summary>
+        /// Overrides of BuildUp method from <see cref="IoC"/> class to work with application container
+        /// </summary>
+        /// <param name="instance">Instance to build</param>
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
+        }
+
+        #endregion
+    }
+}
